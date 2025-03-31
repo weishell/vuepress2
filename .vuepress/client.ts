3da7f576dec26081634b749fafaa62669eb988e3
weishell <38091651+weishell@users.noreply.github.com>
2025-03-31 14:27:05 +0800

    Create client.ts

---
+++ .vuepress/client.ts
@@ -0,0 +1,16 @@
+import { defineClientConfig } from "@vuepress/client"
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+export default defineClientConfig({
+  enhance({ app }) {
+    // 注册ElementPlus
+    app.use(ElementPlus)
+    
+    // 注册图标组件
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component)
+    }
+  },
+})
